16dc81db7cb6553d8ca8b1ed2da6007fb82709a6
Gopi8939 <gopiaswin2002@gmail.com>
2022-02-28 13:13:24 +0530

    hggfjgf

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: const HomePage(
-        animiTime: 70,
+        animiTime: 60,
       ),
     );
   }
